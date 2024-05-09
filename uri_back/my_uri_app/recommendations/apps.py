455a66a73e577bec14cffcf5358b6f4ba1bb1702
djenebaa <djenebasoum98@icloud.com>
2024-05-09 19:54:46 +0200

    refactoring the name of the app recommendation

--- uri_back/my_uri_app/recommendations/apps.py
+++ uri_back/my_uri_app/recommendations/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RecommendationConfig(AppConfig):
+class recommendationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recommendation'
+    name = 'recommendations'

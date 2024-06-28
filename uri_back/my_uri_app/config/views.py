0e85c2d5447ff7fa933504b2654ef479e125705e
djenebaa <djenebasoum98@icloud.com>
2024-06-28 23:13:05 +0200

    Moving home views to auth

--- uri_back/my_uri_app/config/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-@login_required  # This automatically detect is the user is logged in if true redirect to home else it will redirect to login
-def home(request):
-    return render(request, 'home.html', {})

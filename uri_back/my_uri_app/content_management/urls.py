6861ea5543b086deb06b47dd5bf31089a3fe2869
djenebaa <djenebasoum98@icloud.com>
2024-06-29 01:45:41 +0200

    Display tv_show by genre done

--- uri_back/my_uri_app/content_management/urls.py
+++ uri_back/my_uri_app/content_management/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import fetch_external_data, show_genres
 
 urlpatterns = [
-    
-
+    path('external-tv-show/<int:genre_id>/', fetch_external_data, name='tv_show_by_genre'),
+    path('show_genres/', show_genres, name='show_genres'),
 ]

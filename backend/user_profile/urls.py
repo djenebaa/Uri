94bbbaee91cee5a7344fc3445adc62b899ca919b
djenebaa <djenebasoum98@icloud.com>
2024-09-11 00:02:27 +0200

    Create an user profile with favorite list

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import user_profile
 
 urlpatterns = [
-    
-
+    path('', user_profile, name='user_profile'), 
 ]
--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from user_preferences.models import UserFavoriteContent
+from content_management.models import Media
 
-# Create your views here.
+@login_required
+def user_profile(request):
+    favorites = UserFavoriteContent.objects.filter(user=request.user)
+   
+    favorite_media = Media.objects.filter(id__in=[fav.media.id for fav in favorites])
+    
+    return render(request, 'user/profile.html', {
+        'user': request.user,
+        'favorites': favorite_media
+    })

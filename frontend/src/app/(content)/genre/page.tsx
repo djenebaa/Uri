9464f0e0b7b31245665c454124fe686481ee516d
djenebaa <djenebasoum98@icloud.com>
2024-10-07 23:32:19 +0200

    Initialized the genre and profile page

---
+++ frontend/src/app/(content)/genre/page.tsx
@@ -0,0 +1,6 @@
+export default function Genre() {
+    return (
+      <h1 className="text-white"> Here are display all the genre</h1>
+    );
+  }
+  
---
+++ frontend/src/app/(user)/profile/page.tsx
@@ -0,0 +1,6 @@
+export default function Profile() {
+    return (
+      <h1 className="text-white"> Welcome to your Profile</h1>
+    );
+  }
+  

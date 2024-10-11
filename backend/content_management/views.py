bb6051fbc6ca342316e261f0a9da2ea9c4bfe8cd
djenebaa <djenebasoum98@icloud.com>
2024-10-11 19:04:24 +0200

    Remove login required for content

--- backend/content_management/views.py
+++ backend/content_management/views.py
@@ -1,23 +1,19 @@
 import requests
-from django.shortcuts import get_object_or_404, render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from decouple import config
 from content_management.models import Genre, Media
 from user_preferences.models import UserFavoriteContent
 from django.http import JsonResponse
 
-@login_required
+
 def fetch_external_data(request, genre_id=None):
-    # URL de l'API externe
     api_url = "https://api.themoviedb.org/3/discover/tv"
 
-    # En-têtes pour l'API
     headers = {
         "Authorization": f'Bearer {config("TMDB_API_TOKEN")}',
         "Accept": "application/json",
     }
 
-    # Paramètres pour l'API
     params = {
         "include_adult": "false",
         "include_null_first_air_dates": "false",
@@ -27,26 +23,23 @@ def fetch_external_data(request, genre_id=None):
         "with_origin_country": "KR",
     }
 
-    # Ajouter le filtre par genre si le genre_id est spécifié
+    # Add a filter by genre if genre specified
     if genre_id:
         params["with_genres"] = genre_id
 
-    # Faire une requête GET à l'API externe
+    # Get request of the api
     response = requests.get(api_url, headers=headers, params=params)
 
-    # Vérifier si la requête a réussi
+    # Check if the request succeeded
     if response.status_code == 200:
         data = response.json()
         medias = data.get("results", [])
 
         for media in medias:
-            media['external_id'] = media.get('id', None)  
+            media["external_id"] = media.get("id", None)
         return JsonResponse(medias, safe=False)
-    
 
 
-
-@login_required
 def show_type_genres(request):
     api_url = "https://api.themoviedb.org/3/genre/tv/list"
     headers = {
@@ -62,10 +55,9 @@ def show_type_genres(request):
     else:
         genres = []
 
-    return JsonResponse(genres, safe=False) 
+    return JsonResponse(genres, safe=False)
 
 
-@login_required
 def show_media_details(request, external_id):
     try:
         media = Media.objects.get(external_id=external_id)
@@ -102,15 +94,17 @@ def show_media_details(request, external_id):
             )
         else:
             # Redirect or show an error if the external fetch also fails
-            return render(
-                request, "not_found.html", {"message": "Show not found."}
-            )
-            
+            return render(request, "not_found.html", {"message": "Show not found."})
+
     is_favorite = UserFavoriteContent.objects.filter(
-        user=request.user, media=media, media_type='tv_show'
+        user=request.user, media=media, media_type="tv_show"
     ).exists()
 
-    return render(request, "content/content_details.html", {
-        "media": media,
-        "favorites": is_favorite,
-    })
+    return render(
+        request,
+        "content/content_details.html",
+        {
+            "media": media,
+            "favorites": is_favorite,
+        },
+    )

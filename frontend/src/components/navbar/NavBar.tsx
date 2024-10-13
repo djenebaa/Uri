e88a1e2160a248ecf95deb360859512384b2ed1b
djenebaa <djenebasoum98@icloud.com>
2024-10-13 21:16:38 +0200

    Add new style to navbar

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -6,39 +6,42 @@ import { useAuth } from "@/components/auth/AuthenticationContext";
 function NavBar() {
   const { isAuthenticated, username } = useAuth();
   return (
-    <nav className="text-white flex justify-between m-7">
+    <nav className="sticky top-0 left-0 w-full bg-black text-white flex justify-between items-center p-4 z-50"> 
       <AuthChecker />
       <ul className="flex flex-row space-x-5">
-        <li>
-          <Link href="/">Home</Link>
+      <li className="relative group">
+          <Link href="/" className="block px-4 py-2">
+            Home
+          </Link>
+          <span className="absolute left-0 bottom-0 w-full h-1 bg-pink-600 scale-x-0 transition-transform duration-200 group-hover:scale-x-100"></span>
         </li>
-        <li>
-          <Link href="/genre">Genres</Link>
+        <li className="relative group">
+          <Link href="/genre" className="block px-4 py-2">
+            Genre
+          </Link>
+          <span className="absolute left-0 bottom-0 w-full h-1 bg-pink-600 scale-x-0 transition-transform duration-200 group-hover:scale-x-100"></span>
         </li>
-          {/* <li>
-            <Link href="/login">Login</Link>
-          </li> */}
       </ul>
       {isAuthenticated === true && (
         <ul>
-          <li>
-            <Link href="/profile">Profile of {username}!</Link>
+          <li className="relative group">
+            <Link href="/profile" className="block px-4 py-2">
+              Profile of {username}!
+            </Link>
+            <span className="absolute left-0 bottom-0 w-full h-1 bg-pink-600 scale-x-0 transition-transform duration-200 group-hover:scale-x-100"></span>
           </li>
         </ul>
       )}
-
       {isAuthenticated === false && (
         <ul>
-          <li>
-            <Link href="/login">Login</Link>
+          <li className="relative group">
+            <Link href="/login" className="block px-4 py-2">
+              Login
+            </Link>
+            <span className="absolute left-0 bottom-0 w-full h-1 bg-pink-600 scale-x-0 transition-transform duration-200 group-hover:scale-x-100"></span>
           </li>
         </ul>
       )}
-      {/* {isAuthenticated === null && (
-        <ul>
-          <li>Loading...</li>
-        </ul>
-      )} */}
     </nav>
   );
 }
